b3500320edf16b127ca455cd88f342d0e0f24ec0
John Peterson <johndpete5316@outlook.com>
2023-04-19 18:16:17 -0400

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import react from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/App.css'
+import Counter from './components/counter'
+import WeatherCard from './components/WeatherCard'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className='logo-container'>
+          <Counter />
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+      </header>
+      <WeatherCard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/counter.tsx
@@ -0,0 +1,27 @@
+import react, { useState, useEffect } from 'react'
+import '../styles/App.css'
+
+
+function Counter() {
+    const [count, setCount] = useState<number>(() => {
+        const savedCount = localStorage.getItem('count');
+        return savedCount !== null ? Number(savedCount) : 0;
+      });
+    
+      useEffect(() => {
+        localStorage.setItem('count', count.toString());
+      }, [count]);
+
+    return (
+        <div className="Counter">
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
+export default Counter
