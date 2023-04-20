3a185f5d845674582889c399e99d8074f900c6ba
John Peterson <john.peterson@reduxit.net>
2023-04-19 23:54:59 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/App.css'
 import Counter from './components/counter'
-import WeatherCard from './components/WeatherCard'
+import WeatherCard from './components/WeatherApp'
 
 function App() {
   return (
